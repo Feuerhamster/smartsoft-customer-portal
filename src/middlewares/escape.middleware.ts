19e3c6dc5358bd6e4232a8e2da3e46d4a77c4b93
Feuerhamster <feuerhamster02@gmail.com>
2022-01-17 09:59:54 +0100

    revert package files and do custom escape

--- src/middlewares/escape.middleware.ts
+++ src/middlewares/escape.middleware.ts
@@ -1,10 +1,18 @@
 import {Request, Response} from "express";
-import escape from "escape-html";
 
 export default function escapeHTMLMiddleware(req: Request, res: Response, next: any) {
     for (let key in req.body) {
         if (typeof req.body[key] === "string") {
-            req.body[key] = escape(req.body[key]);
+            // escape html control characters
+            req.body[key] = req.body[key].replace(/[&<>"']/g, (c) => {
+                return {
+                    "&": "&amp;",
+                    "<": "&lt;",
+                    ">": "&gt;",
+                    '"': "&quot;",
+                    "'": "&#x27;",
+                }[c];
+            });
         }
     }
 
